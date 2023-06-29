86e7bab17a35e41ec3d68f525e28270a0c712f10
chris <chris.cb.culver@gmail.com>
2023-06-29 12:46:30 +0100

    Trying different setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name='WickContractions',
   version='0.1.0',
   author='Chris Culver',
-  packages=['WickContractions', 'WickContractions.wick'],
+  packages=find_packages('python'),
   license='LICENSE.txt',
   description='Package for wick contractions',
   long_description=open('README.md').read(),
